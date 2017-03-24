e2a492734f00f786b03653baa0c886a5aa276cb5
Lee Saville <l.w.saville@gmail.com>
2017-03-24 14:38:52 -0600

    Add show action

--- web/controllers/todo_controller.ex
+++ web/controllers/todo_controller.ex
@@ -7,4 +7,9 @@ defmodule Todos.TodoController do
     todos = Repo.all(Todo)
     render conn, "index.json", todos: todos
   end
+
+  def show(conn, %{"id" => id}) do
+    todo = Repo.get!(Todo, id)
+    render(conn, "show.json", todo: todo)
+  end
 end
